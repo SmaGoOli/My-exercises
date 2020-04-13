d4cf87d0af2e2d1a35dcd5b68246487aa6bdda6c
Farhad <63376131+SmaGoOli@users.noreply.github.com>
2020-04-13 23:48:09 +0430

    Delete Reverse String Printing.py

--- Reverse String Printing.py
+++
@@ -1,8 +0,0 @@
-def ReverseString(Text):
-    Reverse = list(Text)
-    Reverse.reverse()
-    Reverse_Text = ''.join(Reverse)
-    return Reverse_Text
-    
-while True:
-    print(ReverseString(input('Plz Enter Text :')))
