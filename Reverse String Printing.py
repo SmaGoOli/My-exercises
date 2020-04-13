cab0b0fffbca3bc7c3125170e279f742ab288d99
Farhad <63376131+SmaGoOli@users.noreply.github.com>
2020-04-13 23:50:57 +0430

    Update Reverse String Printing.py

--- Reverse String Printing.py
+++ Reverse String Printing.py
@@ -1,8 +1,8 @@
 def ReverseString(text):
     text_list = list(text)
     text_list.reverse()
-    reverse_text = ''.join(text_list)
-    return reverse_text
+    text_reverse = ''.join(text_list)
+    return text_reverse
     
 while True:
     print(ReverseString(input('Plz Enter Text :')))
